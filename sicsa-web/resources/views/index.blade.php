d886f2691cb8fe6aca57ce65afbb0d593311d30f
SamOctGonzAzpeitia <A01704696@itesm.mx>
2023-05-31 17:48:43 +0200

    star project

---
+++ sicsa-web/resources/views/index.blade.php
@@ -0,0 +1,66 @@
+@extends('layout.blade.php')
+
+@section('seccion')
+
+    
+    
+<div class="mt-5">
+        <div id="carouselExampleControls" class="carousel slide " data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active" data-interval="10000">
+                    <img src="/img/nos2.jpg">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5 style="color: white">Nosotros</h5>
+                        <h6 style="color: white">Con mas de 20 años en el medio, hemos desarrollado distintas aplicaciones administrativas en los diferentes ramos económicos .</h6>
+                    </div>
+                </div>
+                <div class="carousel-item" data-interval="10000">
+                    <img src="/img/system.jpg">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5 style="color: white">Sistemas</h5>
+                        <h6 style="color: white">Deja que nuestras soluciones resuelvan sus problemas de información.</h6>
+                    </div>
+                </div>
+                <div class="carousel-item" data-interval="10000">
+                    <img src="/img/cfdi.png">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5 style="color: black">Actualizate a CFDI 4.0!</h5>
+                        <h6 style="color: black">Evita problemas y actualiza tu sistema con nosotros</h6>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div> 
+    </div>
+
+    <hr>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg">
+                <img src="img/mision.jpg" width="200" height="200" class="bd-placeholder-img rounded-circle">
+                <h2>Misión</h2>
+                <p>Proporcionar servicios integrales de informática a las empresas en general, para la mejora de la calidad de la información de negocio que les permita la toma oportuna de decisiones.</p>
+                
+            </div>
+            
+            <div class="col-lg">
+                
+                <img src="img/vision.jpg" width="200" height="200" class="bd-placeholder-img rounded-circle">
+                <h2>Visión</h2>
+                <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
+                
+            </div>
+        </div>
+    </div>
+    
+
+@endsection
+    
---
+++ sicsa-web/resources/views/layout.blade.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <script src="https://kit.fontawesome.com/76fa277871.js" crossorigin="anonymous"></script>
+    
+    
+    <title>Sicsa</title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-md navbar-light bg-light mb-5">
+    <div class="container-fluid">
+      <img  src="img/logo.png" width="150" height="90" href="#">
+      <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="navbar-collapse collapse" id="navbarCollapse" style="">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+            <li class="nav-item px-2 mr-auto">
+                    <a class="nav-link h5" href="{{ route('home') }}">Inicio</a>
+                </li>
+                <li class="nav-item px-2">
+                    <a class="nav-link h5"  href="{{ route('services') }}">Servicios</a>
+                </li>
+                <li class="nav-item px-2">
+                    <a class="nav-link h5" href="{{ route('systems') }}">Sistemas</a>
+                </li>
+                <li class="nav-item px-2">
+                    <a class="nav-link h5" href="{{ route('contact') }}">Contacto</a>
+                </li>
+                <!-- <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" style="color:black" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="fas fa-user-circle"></i>
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown" hidden>
+                        <li><a class="dropdown-item" href="{{ route('login') }}">Iniciar Sesión</a></li>
+                        
+                        
+                    </ul> -->
+            </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  
+  <div class="container mt-5">
+    @yield('seccion')
+  </div>  
+      
+  <hr>
+  <footer class="m-3">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <p><i class="bi bi-envelope"></i> sicsa2004@prodigy.net.mx</p>
+            <p><i class="bi bi-phone"></i> 442-215-38-73</p>
+          </div>
+          <div class="col">
+            <p><i class="bi bi-facebook"></i> <a href= "https://www.facebook.com/sicsa.sicsa.7731?ref"> Sicsa</p>
+            <p><i class="bi bi-whatsapp"></i> <a href="https://wa.link/ihxwkd"> 442-716-04-38</p>
+          </div>
+        </div>
+      </div>
+  </footer>
+
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    
+    
+</body>
+</html>
